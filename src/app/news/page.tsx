1c0f9c20709771128f0b107bf865092191220376
it-seablueshipyard <it@seablueshipyard.com>
2026-09-19 15:51:50 +0530

    Update page.tsx

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -9,7 +9,8 @@ export const metadata: Metadata = {
 };
 
 const NEWSLETTERS = [
-  { label: "E-Newsletter - Jun26 Jul26 ", href: "/documents/newsletters/SBSL Newsletter Jun26 Jul26 .pdf" },
+  { label: "E-Newsletter - Jun26 Jul26 ", href: "/documents/newsletters/SBSL Newsletter APR 26-MAY 26.pdf" },
+  { label: "E-Newsletter - Apr26 May26 ", href: "/documents/newsletters/SBSL Newsletter Jun26 Jul26 .pdf" },
   { label: "E-Newsletter - Feb26 Mar26 ", href: "/documents/newsletters/SBSL-Newsletter-FEB26'-MAR'26.pdf" },
   { label: "E-Newsletter - Dec25 Jan26 ", href: "/documents/newsletters/SBSL-Newsletter-DEC'25-JAN'26.pdf" },
   { label: "E-Newsletter - Oct25 Nov25 ", href: "/documents/newsletters/SBSL Newsletter OCT 25-NOV 25.pdf" },
